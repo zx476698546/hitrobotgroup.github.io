03657d6aa8caf223a34bc0bd310ff7ede63ce796
ros-org <31200535+ros-org@users.noreply.github.com>
2017-12-08 15:30:36 +0800

    添加pdf提示等功能

--- js/index.js
+++ js/index.js
@@ -54,21 +54,65 @@ $(function (){
     $("#pdfClick").on('click', function () {
     	if(i%2 == 1){  
 	    	$("#pdf").css("width", "100%");
-	    	$(this).text(" ").css("width", "30px").css("background-image", "url(image/left.png)").css("background-size", "75%").css("right", 0).css("border", "none").css("border-left", "2px solid #4184e2")
+	    	$(this).text(" ").css("width", "30px").css("background-image", "url(image/left.png)").css("background-size", "75%").css("right", 0).css("border", "1px #ccc solid").css("background-position", "center")
     	} else if(i%2 == 0){ 
 	    	$("#pdf").css("width", "130px");
-	    	$(this).text("查看PDF文档").css("width", "130px").css("background-image", "url(image/right.png)").css("background-size", "17.5%").css("border", "none").css("border-right", "2px solid #4184e2")	
+	    	$(this).text("查看PDF文档").css("width", "130px").css("background-image", "url(image/right.png)").css("background-size", "17.5%").css("border", "none").css("background-position", "right")
     	};
     	i ++ 
     })
     $("#wordClick").on('click', function () {
     	if(i%2 == 1){  
 	    	$("#word").css("width", "100%");
-	    	$(this).text(" ").css("width", "20px").css("background-image", "url(image/left.png)").css("background-size", "90%").css("right", 0)
+	    	$(this).text(" ").css("width", "20px").css("background-image", "url(image/left.png)").css("background-size", "90%").css("right", 0).css("border", "1px #ccc solid").css("background-position", "center")
     	} else if(i%2 == 0){ 
 	    	$("#word").css("width", "80px");
-	    	$(this).text("查看文档").css("width", "80px").css("background-image", "url(image/right.png)").css("background-size", "17.5%")
+	    	$(this).text("查看文档").css("width", "80px").css("background-image", "url(image/right.png)").css("background-size", "17.5%").css("border", "none").css("background-position", "right")
     	};
     	i ++ 
     })
+    //pdf
+    $(".pdf1").on('mouseover', function () {
+    	$("#pdfName").text("蓄电池托盘搬运车 T20AP04")
+    })
+    $(".pdf2").on('mouseover', function () {
+    	$("#pdfName").text("站驾式电动托盘搬运叉车 T20SP")
+    }) 
+    $(".pdf3").on('mouseover', function () {
+    	$("#pdfName").text("站驾式电动托盘堆垛叉车")
+    }) 
+    $(".pdf4").on('mouseover', function () {
+    	$("#pdfName").text("自动导引车通用技术条件")
+    }) 
+    $(".pdf5").on('mouseover', function () {
+    	$("#pdfName").text("自动导引车(AGV)设计通则")
+    }) 
+    $(".pdf6").on('mouseover', function () {
+    	$("#pdfName").text("自动导引车(AGV) 术语")
+    })    
+    $("#pdf li").on('mouseout', function () {
+    	$("#pdfName").text(" ")
+    })  
+    //word
+    $(".word1").on('mouseover', function () {
+    	$("#wordName").text("蓄电池托盘搬运车 T20AP04")
+    })
+    $(".word2").on('mouseover', function () {
+    	$("#wordName").text("站驾式电动托盘搬运叉车 T20SP")
+    }) 
+    $(".word3").on('mouseover', function () {
+    	$("#wordName").text("站驾式电动托盘堆垛叉车")
+    }) 
+    $(".word4").on('mouseover', function () {
+    	$("#wordName").text("自动导引车通用技术条件")
+    }) 
+    $(".pword5").on('mouseover', function () {
+    	$("#wordName").text("自动导引车(AGV)设计通则")
+    }) 
+    $(".word6").on('mouseover', function () {
+    	$("#wordName").text("自动导引车(AGV) 术语")
+    })    
+    $("#word li").on('mouseout', function () {
+    	$("#wordName").text(" ")
+    }) 
 });
